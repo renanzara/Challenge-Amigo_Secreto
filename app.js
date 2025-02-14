9f457c1e07520564f9e768b4abf6aaa5e9831ae9
renanzara <renanzara18@hotmail.com>
2025-02-14 16:23:42 -0300

    Função atualizar lista

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ let amigos = [];
 function adicionarAmigo() {
     let inputAmigo = document.getElementById('amigo');
     let novoAmigo = inputAmigo.value;
+    
     if (novoAmigo.trim() == "") {
         alert('Por favor, insira um nome');
         return;
     } else {
         amigos.push(novoAmigo);
         inputAmigo.value = "";
+        atualizarLista()
+    }
+}
+
+function atualizarLista() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    for (let amigo of amigos) {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        listaAmigos.appendChild(item);
     }
 }
